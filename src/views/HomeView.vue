bff63758b9925ac2f2da4be27e01414d4c2e35f5
Antony-Anderson <antony01anderson@gmail.com>
2024-07-31 16:28:41 -0300

    projeto base

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <p>Estou aqui e esta funcionando</p>
+    <TheWelcome />
+  </main>
+</template>
